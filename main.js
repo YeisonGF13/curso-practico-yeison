97726f9be3f73a78e06790ef8bf5289a0bd4dcc0
Yeison Giraldo <tiiven.m10@gmail.com>
2023-05-11 17:00:11 -0500

    Nav Bar terminada, empezando section y js

--- main.js
+++ main.js
@@ -1,17 +1,95 @@
+// JS DE LA NAVBAR Y LA SIDEBAR RESPOSIVE
+
 const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.inactive');
+const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamburguesa = document.querySelector('.menu');
-const mobileMenu = document.querySelector('.mobile-inactive');
+const mobileMenu = document.querySelector('.mobile-menu');
+const carrito = document.querySelector('.navbar-shopping-cart');
+const productDetail = document.querySelector('.product-detail');
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamburguesa.addEventListener('click', toggleMobileMenu);
+carrito.addEventListener('click', toggleCarrito);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
 }
 
-
-
-menuHamburguesa.addEventListener('click', toggleMobileMenu);
-
 function toggleMobileMenu() {
-    mobileMenu.classList.toggle('mobile-inactive');
+    const isCarritoClosed = productDetail.classList.contains('inactive');
+
+    if (!isCarritoClosed) {
+        productDetail.classList.add('inactive')
+    }
+
+    mobileMenu.classList.toggle('inactive')
+
 }
+
+function toggleCarrito() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive')
+    }
+
+    productDetail.classList.toggle('inactive');
+    desktopMenu.classList.add('inactive');
+}
+
+// JS DE LA NAVBAR Y LA SIDEBAR RESPOSIVE
+
+// JS DEL SECTION
+
+const productList = []
+
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+
+productList.push({
+    name: 'Pantalla',
+    price: 220,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+
+productList.push({
+    name: 'Compu',
+    price: 620,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+
+/*
+<div class="product-card">
+<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    alt="">
+<div class="product-info">
+    <div>
+        <p>$120,00</p>
+        <p>Bike</p>
+    </div>
+    <figure>
+        <img src="./icons/bt_add_to_cart.svg" alt="">
+    </figure>
+</div>
+</div>
+*/
+
+
+for (product of productList) {
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+
+    const img = document.createElement('img');
+    img.setAttribute('src', product.image)
+
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+
+    const productName = document.createElement('p')
+    const productPrice = document.createElement('p')
+}
+
+// JS DEL SECTION
