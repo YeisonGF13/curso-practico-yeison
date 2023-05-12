a82fed3f1bf27fcab9c69f3218335903e54f9588
Yeison Giraldo <tiiven.m10@gmail.com>
2023-05-12 14:30:37 -0500

    Termine el js para que al seleccionar una imagen aparezca su descripción

--- main.js
+++ main.js
@@ -1,19 +1,30 @@
-// JS DE LA NAVBAR Y LA SIDEBAR RESPOSIVE
-
+// CREANDO Y DEFINIENDO VARIABLES
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamburguesa = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const carrito = document.querySelector('.navbar-shopping-cart');
 const productDetail = document.querySelector('.product-detail');
+const productDetailContainer = document.querySelector('.product-detail-detalle')
+const productDetailCloseIcon = document.querySelector('.product-detail-close')
 
 
+// AGREGANDO EL EVENTO DE CLICK JUNTO CON LAS FUNCIONES
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamburguesa.addEventListener('click', toggleMobileMenu);
 carrito.addEventListener('click', toggleCarrito);
+productDetailCloseIcon.addEventListener('click', addproductDetailCloseIcon)
 
+// CREANDO LAS FUNCIONES QUE SE ENCARGAN DE OCULTAR LOS MENUS DESPLEGABLES CUANDO SE ABRE OTRO
 function toggleDesktopMenu() {
+    const productDetailClosed = productDetail.classList.contains('inactive')
+
+    if (!productDetailClosed) {
+        productDetail.classList.add('inactive')
+    }
     desktopMenu.classList.toggle('inactive');
+
+
 }
 
 function toggleMobileMenu() {
@@ -23,7 +34,9 @@ function toggleMobileMenu() {
         productDetail.classList.add('inactive')
     }
 
-    mobileMenu.classList.toggle('inactive')
+    else {
+        mobileMenu.classList.toggle('inactive')
+    }
 
 }
 
@@ -34,14 +47,32 @@ function toggleCarrito() {
         mobileMenu.classList.add('inactive')
     }
 
-    productDetail.classList.toggle('inactive');
-    desktopMenu.classList.add('inactive');
+    else {
+        productDetail.classList.toggle('inactive');
+        desktopMenu.classList.add('inactive');
+        productDetailContainer.classList.add('inactive')
+    }
+
+
 }
 
-// JS DE LA NAVBAR Y LA SIDEBAR RESPOSIVE
+function addproductDetailCloseIcon() {
+    productDetailContainer.classList.add('inactive');
+}
 
-// JS DEL SECTION
+function openProductDetailAside() {
+    const productDetailClosed = productDetail.classList.contains('inactive')
 
+    if (!productDetailClosed) {
+        productDetail.classList.add('inactive')
+    }
+
+    productDetailContainer.classList.remove('inactive');
+
+}
+
+
+// CREANDO UN ARRAY DONDE SE VAN A CREAR LOS ELEMENTOS DE VENTA COMO LA IMAGEN, NOMBRE Y PRECIO
 const productList = []
 
 productList.push({
@@ -62,7 +93,7 @@ productList.push({
     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
 });
 
-
+// FUNCION DONDE SE CREAN LOS ELEMENTOS HTML
 function productos(array) {
 
     for (product of array) {
@@ -73,6 +104,7 @@ function productos(array) {
 
         const productImg = document.createElement('img');
         productImg.setAttribute('src', product.image)
+        productImg.addEventListener('click', openProductDetailAside)
 
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
@@ -102,11 +134,8 @@ function productos(array) {
         productCard.appendChild(productInfo)
 
         cardsContainer.appendChild(productCard)
-
-
-
     }
 }
 
+// ACÁ SE EJECUTA LA FUNCION PASANDOLE COMO PARAMETRO EL ARRAY PRODUCTLIST
 productos(productList)
-// JS DEL SECTION
