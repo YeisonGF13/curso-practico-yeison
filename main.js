d68c9f98027d5cc22493865b0e5fbd19536a7f98
Yeison Giraldo <tiiven.m10@gmail.com>
2023-05-12 09:24:32 -0500

    Termine de maquetar la imagen, nombre y precio del producto con js en el main.js

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const mobileMenu = document.querySelector('.mobile-menu');
 const carrito = document.querySelector('.navbar-shopping-cart');
 const productDetail = document.querySelector('.product-detail');
 
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamburguesa.addEventListener('click', toggleMobileMenu);
 carrito.addEventListener('click', toggleCarrito);
@@ -47,49 +48,65 @@ productList.push({
     name: 'Bike',
     price: 120,
     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-})
+});
 
 productList.push({
     name: 'Pantalla',
     price: 220,
     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-})
+});
 
 productList.push({
     name: 'Compu',
     price: 620,
     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-})
-
-/*
-<div class="product-card">
-<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-    alt="">
-<div class="product-info">
-    <div>
-        <p>$120,00</p>
-        <p>Bike</p>
-    </div>
-    <figure>
-        <img src="./icons/bt_add_to_cart.svg" alt="">
-    </figure>
-</div>
-</div>
-*/
+});
 
 
-for (product of productList) {
-    const productCard = document.createElement('div');
-    productCard.classList.add('product-card');
+function productos(array) {
 
-    const img = document.createElement('img');
-    img.setAttribute('src', product.image)
+    for (product of array) {
+        const cardsContainer = document.querySelector('.cards-container');
 
-    const productInfo = document.createElement('div');
-    productInfo.classList.add('product-info');
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
 
-    const productName = document.createElement('p')
-    const productPrice = document.createElement('p')
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image)
+
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+
+        const productName = document.createElement('p')
+        productName.innerText = '$' + product.name;
+
+        const productPrice = document.createElement('p')
+        productPrice.innerText = '$' + product.price;
+
+        const productInfoDiv = document.createElement('div')
+
+        productInfoDiv.appendChild(productName)
+        productInfoDiv.appendChild(productPrice)
+
+
+        const productInfoFigure = document.createElement('figure')
+        const productImgCart = document.createElement('img')
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg')
+
+        productInfoFigure.appendChild(productImgCart);
+
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+
+        productCard.appendChild(productImg)
+        productCard.appendChild(productInfo)
+
+        cardsContainer.appendChild(productCard)
+
+
+
+    }
 }
 
+productos(productList)
 // JS DEL SECTION
